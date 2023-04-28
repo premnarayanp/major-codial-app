2391fd15dbc6f8c4d8fdfba9d7a39d320d5ebdc2
premnarayanp <premnarayanpatel223@gmail.com>
2023-04-28 11:19:09 +0530

    costumFetch created in src/api/index.js for req & res

---
+++ src/api/index.js
@@ -0,0 +1,48 @@
+import { API_URLS, LOCALSTORAGE_TOKEN_KEY } from '../utils';
+
+const customFetch = async (url, { body, ...customConfig }) => {
+  const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+
+  const headers = {
+    'content-type': 'application/json',
+    Accept: 'application/json',
+  };
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  const config = {
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
+  };
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+
+  try {
+    const response = await fetch(url, config);
+    const data = await response.json();
+
+    if (data.success) {
+      return {
+        data: data.data,
+        success: true,
+      };
+    }
+
+    throw new Error(data.message);
+  } catch (error) {
+    console.error('error');
+    return {
+      message: error.message,
+      success: false,
+    };
+  }
+};
+
+
