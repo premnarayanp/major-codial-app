2e622fe4f8197c92d9aecaec2ec6bf06bd57fcd0
premnarayanp <premnarayanpatel223@gmail.com>
2023-05-03 14:20:11 +0530

    solve problem ,when refresh then user name not update from localstorage

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -36,10 +36,10 @@ export const useProvideAuth = () => {
     console.log('response', response);
     if (response.success) {
       setUser(response.data.user);
-      // setItemInLocalStorage(
-      //   LOCALSTORAGE_TOKEN_KEY,
-      //   response.data.token ? response.data.token : null
-      // );
+      setItemInLocalStorage(
+        LOCALSTORAGE_TOKEN_KEY,
+        response.data.token ? response.data.token : null
+      );
       return {
         success: true,
       };
